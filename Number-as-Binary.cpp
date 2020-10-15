1786f4c6f7bc571107cb2da4d3df65e11b60b36d
TONY <tony.shcherba@gmail.com>
2020-10-15 12:13:57 +0300

    feat: function implementation "NumberAsBinary"
    decimal to binary conversion

--- Number-as-Binary.cpp
+++ Number-as-Binary.cpp
@@ -1,6 +1,14 @@
 ﻿#include <iostream>
 using namespace std;
 
+void NumberAsBinary(char* _result, unsigned int _number) {
+    static unsigned int i;
+    i = 0;
+    if (_number / 2 != 0) NumberAsBinary(_result, _number / 2);
+    _result[i] = _number % 2 == 0 ? '0' : '1';
+    _result[++i] = '\0';
+}
+
 int main()
 {
     cout << "Hello World!\n";
